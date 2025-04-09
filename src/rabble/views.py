d4d715d710cad62641fbdf703a1aaea4fd4f38d9
root <root@NamsComputer.>
2025-04-09 17:42:16 -0500

    done probably

--- src/rabble/views.py
+++ src/rabble/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context = {"welcome": "Hello, world!"}
+    if request.user.is_authenticated:
+        context = {"welcome": str(request.user) + "!"}
+    else:
+        context = {"welcome": "to Rabble!"}
 
     return render(request, "rabble/index.html", context)
 
 def profile(request):
-    return render(request, "rabble/profile.html")
+    context = {"username" : str(request.user), "email": str(request.user) + "@example.com"}
+    
+    return render(request, "rabble/profile.html", context)
