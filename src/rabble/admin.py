70c2b40ac6b93367b8cabd3b48c5b3a8eeaea715
root <root@NamsComputer.>
2025-04-16 14:49:42 -0500

    admin site stuff

--- src/rabble/admin.py
+++ src/rabble/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Community, subRabble, Post, Comment, Conversation, Message, Follow
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Community)
+admin.site.register(subRabble)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(Follow)
