8e5f79231d9a1a40bdd0a7b085c3195038cd8f6e
root <root@NamsComputer.>
2025-05-13 23:22:24 -0500

    api urls.py changes

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('users/', views.user_list, name='api-user-list'),
-    path('subRabbles/', views.subRabble_list, name='api-subRabble-list'),
-    path('subRabbles/!<str:identifier>/posts/', views.post_list, name='api-post-list'),
+    path('subrabbles/', views.subRabble_list, name='api-subRabble-list'),
+    path('subrabbles/!<str:identifier>/posts/', views.post_list, name='api-post-list'),
     
     path('users/<int:pk>/', views.user_detail, name='api-user-detail'),
-    path('subRabbles/!<str:identifier>/', views.subRabble_detail, name='api-subRabble-detail'),
-    path('subRabbles/!<str:identifier>/posts/<int:pk>/', views.post_detail, name='api-post-detail'),
-    path("subRabbles/!<str:identifier>/posts/<int:pk>/likes", views.toggle_like, name="post-like-toggle"),
+    path('subrabbles/!<str:identifier>/', views.subRabble_detail, name='api-subRabble-detail'),
+    path('subrabbles/!<str:identifier>/posts/<int:pk>/', views.post_detail, name='api-post-detail'),
+    path("subrabbles/!<str:identifier>/posts/<int:pk>/likes", views.toggle_like, name="post-like-toggle"),
 ]
